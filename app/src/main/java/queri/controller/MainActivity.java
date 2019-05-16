65f6622e1c044d13fd966cc2d76694b57d69730f
gsvetleen <gsvetleen@gmail.com>
2019-05-16 06:35:07 -0400

    Added Logout button

--- app/src/main/java/queri/controller/MainActivity.java
+++ app/src/main/java/queri/controller/MainActivity.java
@@ -1,16 +1,22 @@
 package queri.controller;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.capstone.queri.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +30,56 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(navlistener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeaturedFragment())
                 .commit();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.logout, menu);// Menu Resource, Menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.action_maintain:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle("Confirm");
+                builder.setMessage("Are you sure?");
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        try{
+                            FirebaseAuth.getInstance().signOut();
+                            dialog.dismiss();
+                            Intent myIntent = new Intent(MainActivity.this, LoginPage.class);
+                            MainActivity.this.startActivity(myIntent);
+                        }catch(Exception e){
+                            Toast.makeText(getApplicationContext(),"Unable to Log out",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Do nothing
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     public void replies(@Nullable Bundle bundle)
     {
